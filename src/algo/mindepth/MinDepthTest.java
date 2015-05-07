549d67385159fc527cf3fc74b3648f6fe9419b3e
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2015-05-07 08:38:14 +0200

    Write tests. Some of them fail

--- src/algo/mindepth/MinDepthTest.java
+++ src/algo/mindepth/MinDepthTest.java
@@ -16,7 +16,13 @@ public class MinDepthTest {
     @Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-                { fillTestTree(), 1 }
+                { fillSingleLeaf(), 0 },
+                { fillTwoLeavesBalanced(), 1 },
+                { fillLeftSubtree(), 0 },
+                { fillBalancedTwoLevels(), 2 },
+                { fillRightSubtree(), 0 },
+                { fillLeftPath(), 0 },
+                { fillRightPath(), 0 }
         });
     }
 
@@ -31,20 +37,114 @@ public class MinDepthTest {
 
     @Test
     public void test() {
-
+        assertEquals(fExpected, fInput.getMinDepthBfs());
         assertEquals(fExpected, fInput.getMinDepth());
     }
 
-    private static Tree fillTestTree() {
-        Tree tree = new Tree(100);
-        tree.insert(10);
-        tree.insert(5);
-        tree.insert(70);
-        tree.insert(1);
+    /*
+     *          10
+     */
+    private static Tree fillSingleLeaf() {
+        Tree tree = new Tree(10);
+        return tree;
+    }
+    
+    /*
+     *          10
+     *        /    \
+     *       3     13
+     */
+    private static Tree fillTwoLeavesBalanced() {
+        Tree tree = new Tree(10);
+        tree.insert(3);
+        tree.insert(13);
+        return tree;
+    }
+    
+    /*
+     *          10
+     *         /
+     *        3
+     *       / \
+     *      2   7
+     *     /
+     *    1
+     */
+    private static Tree fillLeftSubtree() {
+        Tree tree = new Tree(10);
+        tree.insert(3);
         tree.insert(7);
-        tree.insert(6);
-        tree.insert(8);
-        tree.insert(150);
+        tree.insert(2);
+        tree.insert(1);
+        return tree;
+    }
+    
+
+    /*
+     *      10
+     *        \
+     *        14
+     *        / \
+     *       12  16
+     */
+    private static Tree fillRightSubtree() {
+        Tree tree = new Tree(10);
+        tree.insert(13);
+        tree.insert(14);
+        tree.insert(12);
+        tree.insert(16);
+        return tree;
+    }
+    
+    /*
+     *         10
+     *        /  \
+     *       7    13
+     *      / \   / \
+     *     2   9 12  15
+     */
+    private static Tree fillBalancedTwoLevels() {
+        Tree tree = new Tree(10);
+        tree.insert(13);
+        tree.insert(7);
+        tree.insert(2);
+        tree.insert(9);
+        tree.insert(12);
+        tree.insert(15);
+        return tree;
+    }
+    
+    /*
+     *          10
+     *         /
+     *        7
+     *       /
+     *      2
+     *     /
+     *    1 
+     */
+    private static Tree fillLeftPath() {
+        Tree tree = new Tree(10);
+        tree.insert(7);
+        tree.insert(2);
+        tree.insert(1);
+        return tree;
+    }
+    
+    /*
+     *         10
+     *           \
+     *            15
+     *              \
+     *               17
+     *                \
+     *                 21
+     */
+    private static Tree fillRightPath() {
+        Tree tree = new Tree(10);
+        tree.insert(15);
+        tree.insert(17);
+        tree.insert(21);
         return tree;
     }
 
